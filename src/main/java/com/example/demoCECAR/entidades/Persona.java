edbace5acab8ed9a730be713907f612074effce7
Rober Sehuanez <sehuanez@gmail.com>
2025-05-13 10:11:22 -0500

    Creando entidad persona

---
+++ src/main/java/com/example/demoCECAR/entidades/Persona.java
@@ -0,0 +1,35 @@
+package com.example.demoCECAR.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "personas")
+@AllArgsConstructor
+@Getter
+@Builder
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "cedula")
+    private int cedula;
+
+}
