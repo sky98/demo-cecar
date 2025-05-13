f039081bc2ca21e2509fd84890269db6f3bd5415
Rober Sehuanez <sehuanez@gmail.com>
2025-05-13 10:00:59 -0500

    Primer commit
    - Configuracion inicial del proyecto

---
+++ src/main/java/com/example/demoCECAR/DemoCecarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCECAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoCECAR/DemoCecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCECAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
